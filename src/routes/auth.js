14511dbd1b76ee127bbdf54e94a8a66ce2034a73
Sophonyas Bewuketu <contactsophonyas@gmail.com>
2026-05-26 09:55:00 +0300

    Transported for Otp added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,12 +3,16 @@ const pool = require('../config/db');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-// Email transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
